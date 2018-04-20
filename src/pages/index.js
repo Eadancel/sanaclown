5287f6d6332ba804bbc64bfb0a2d404683cb4ddc
Leopoldo Rosas <polo@MacBook-Air.local>
2018-04-19 23:31:58 -0300

    reupload

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ class IndexPage extends React.Component {
           titleColor="#D0021B"
           title="Quienes Somos"
           Phrase="La sonrisa es una verdadera fuerza vital, la única capaz de mover lo inconmovible"
-          ContentText="Sanaclown es una organización multidisciplinaria dedicada al clown terapéutico de hospital.<br>
+          ContentText="Sanaclown es una organización multidisciplinaria dedicada al clown terapéutico de hospital.
           A través de la risa, la música, el juego, la imaginación, magia, lenguaje psicoterapeutico, el uso de titere y juguete terapeutico, contribuye en el proceso de restablecimiento del bienestar integral de las personas, entregando alegría y desdramatizando el ambiente hospitalario.
           Sanaclown se sustenta sobre el convencimiento que la alegría y la risa son un camino certero para potenciar la existencia de individuos y grupos humanos más felices, equilibrados y activos.
           "
