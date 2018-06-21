a6773e284dda0d8f9ef8933afa56be0ed6bc7c13
Leopoldo Rosas <polo@MacBook-Air.local>
2018-06-21 00:57:44 -0400

    -blog -new attrbs

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -9,20 +9,95 @@ const CPwrp = styled.div`
     padding-top:271px;
 `;
 
+const CardWrp = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+
+        @media screen and (max-width: $mobile) {
+            flex-direction: column;
+        }
+
+`;
+const BlogCard = styled.div`
+        height: 515px;
+        width: 368px;
+        margin-left: 15px;
+        margin-right: 15px;
+`;
+const BlogThumbNail = styled.div`
+    width: 368px;
+    height: 287px;
+    object-fit: cover;
+`;
+const BlogDetails = styled.div`
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 20px;
+    background: $color-white;
+    box-shadow: 0 1px 7px rgba(0,0,0,0.5);
+`;
+const BlogTitle = styled.div`
+
+    font-family: Verdana;
+    font-size: 25px;
+    color: #303030;
+    letter-spacing: -0.6px;
+    text-align: left;
+    line-height: 30px;
+`;
+const BlogDescription = styled.div`
+
+    font-family: Verdana;
+    font-size: 16px;
+    color: #303030;
+    letter-spacing: -0.38px;
+    text-align: left;
+    line-height: 22px;
+`;
+
+const ButtonAction = styled.button`
+    background: transparent;
+    border: none;
+    font-family: 'Quantico';
+    font-weight: 700;
+    text-transform: uppercase;
+    text-align: left;
+    color: $color-gray;
+    cursor: pointer;
+`;
+
 const Blog = props => (
     <CPwrp>
         <h4>{props.posts.totalCount} Posts</h4>
         {console.log(props.posts)}
-        <ul style={{marginLeft: '0px'}}>
+        <CardWrp>
+
+
+
          {props.posts.edges.map(({node}) =>
 
-                <li style={{listStyle: 'none'}}
-                    key={node.id}>
+                <BlogCard key={node.id}>
+                    <BlogThumbNail>
+                        <img src="http://placeimg.com/368/287/nature" />
+                    </BlogThumbNail>
                     {console.log(node)}
-                    {node.frontmatter.title}
-                </li>
+                    <BlogDetails>
+                        <BlogTitle>
+                            {node.frontmatter.title}
+                        </BlogTitle>
+
+                        <BlogDescription>
+                            Flat Chrysler Automobiles is making Mopar Crate Hemi Engine             kits available as a factory-backed option for Mopar vehicles             from 1975 and earlier.
+                        </BlogDescription>
+                    </BlogDetails>
+              </BlogCard>
+
             )}
-         </ul>
+
+         </CardWrp>
     </CPwrp>
 
 )
